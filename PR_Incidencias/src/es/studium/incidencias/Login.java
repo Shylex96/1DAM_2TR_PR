9cc201d43e650585f115263940b2db29b618ad9d
Shylex96 <77785532+Shylex96@users.noreply.github.com>
2023-03-01 15:41:32 +0100

    Update Login.java

--- PR_Incidencias/src/es/studium/incidencias/Login.java
+++ PR_Incidencias/src/es/studium/incidencias/Login.java
@@ -9,33 +9,39 @@ import java.awt.Label;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
-public class Login implements ActionListener, WindowListener {
-	
+public class Login implements ActionListener, WindowListener, KeyListener {
+
 	Frame windowLogin = new Frame("Login");
 	Dialog dlgWindow = new Dialog(windowLogin, "Error", true);
-	
+
 	Label lblUsuario = new Label ("Usuario:");
 	Label lblClave = new Label ("Clave:    ");
 	Label lblMensaje = new Label ("Credenciales incorrectas.");
-	
+
 	TextField txtUsuario = new TextField(10);
 	TextField txtClave = new TextField(10);
-	
+
 	Button btnAcceder = new Button("Acceder");
 	Conexion conexion = new Conexion ();
-	
+
 	Login() {
-		
+
 		windowLogin.setLayout(new FlowLayout());
 		windowLogin.addWindowListener(this);
 		windowLogin.setResizable(false);
 		windowLogin.setSize(220, 130); 
 		windowLogin.setLocationRelativeTo(null);
 		windowLogin.setBackground(Color.orange);
-		
+
+		// Escuchar para tecla enter:
+		txtUsuario.addKeyListener((KeyListener) this);
+		txtClave.addKeyListener((KeyListener) this);
+
 		windowLogin.add(lblUsuario);
 		//txtUsuario.setForeground(Color.red);
 		windowLogin.add(txtUsuario);
@@ -45,10 +51,10 @@ public class Login implements ActionListener, WindowListener {
 		windowLogin.add(txtClave);
 		windowLogin.add(btnAcceder);
 		btnAcceder.addActionListener(this);
-		
+
 		windowLogin.setVisible(true);
 	}
-	
+
 
 	public static void main(String[] args) {
 		new Login();
@@ -81,7 +87,7 @@ public class Login implements ActionListener, WindowListener {
 		{
 			String usuario = txtUsuario.getText();
 			String clave = txtClave.getText();
-			
+
 			// Credenciales:
 			if (conexion.comprobarCredenciales(usuario, clave)) {
 				new MenuPrincipal();
@@ -97,11 +103,52 @@ public class Login implements ActionListener, WindowListener {
 				dlgWindow.add(lblMensaje);
 				txtUsuario.setText("");
 				txtClave.setText("");
-				
+
 				dlgWindow.setVisible(true);
 			}
-			
+
 
 		}
+
+
+
 	}
+
+
+	@Override
+	public void keyTyped(KeyEvent e){}
+	@Override
+	public void keyReleased(KeyEvent e){}
+	@Override
+	public void keyPressed(KeyEvent e)
+	{
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+			String usuario = txtUsuario.getText();
+			String clave = txtClave.getText();
+
+			// Credenciales:
+			if (conexion.comprobarCredenciales(usuario, clave)) {
+				new MenuPrincipal();
+				windowLogin.setVisible(false);
+			}
+			else {
+				dlgWindow.setLayout(new FlowLayout());
+				dlgWindow.addWindowListener(this);
+				dlgWindow.setResizable(false);
+				dlgWindow.setLocationRelativeTo(null);
+				dlgWindow.setSize(220, 80); 
+				dlgWindow.setBackground(Color.orange);
+				dlgWindow.add(lblMensaje);
+				txtUsuario.setText("");
+				txtClave.setText("");
+
+				dlgWindow.setVisible(true);
+			}
+		}
+
+	}
+
 }
+
+
+
